93db17890e825f0923077cad919c13fd78006e3e
mohammad khedmati <mohammadkhedmat@mohammads-MacBook-Pro.local>
2019-04-19 13:31:24 +0430

    Robo Friends Add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import './App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+// 					<Card id={robots.id[0]} name="{robots.name[0]}" email="{robots.email[0]}"/>
